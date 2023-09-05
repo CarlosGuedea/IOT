ec8c2763ee9d315569b2f1fbf6510f2098acf853
CarlosGuedea <57150568+CarlosGuedea@users.noreply.github.com>
2023-09-05 15:07:17 -0600

    Control de errores

---
+++ Unidad2/Errores.py
@@ -0,0 +1,73 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+contador = 11
+
+class MyHTTPRequestHandler(BaseHTTPRequestHandler):
+    def throw_custom_error(self,message):
+        self._set_response("application/json")
+        self.wfile.write(json.dumps({"message": message}).encode())
+        
+    def _set_response(self, content_type="text/plain"):
+        self.send_response(200)
+        self.send_header("Content-type", content_type)
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_response()
+        respuesta = "El valor es: " + str(contador)
+        self.wfile.write(respuesta.encode())
+
+    def do_POST(self):
+        content_length = int(self.headers["Content-Length"])
+        post_data = self.rfile.read(content_length)
+        
+        try:
+            body_json = json.loads(post_data.decode())
+        except:
+            self._set_response("application/json")
+            self.wfile.write(json.dumps({"message":" Invalid JSON"}).encode())
+            return
+
+        global contador
+        
+        #Check if action and quantity are present
+        if(body_json.get('action') is None or body_json.get('quantity') is None):
+            self.throw_custom_error("Invalid JSON")
+            return
+        
+            
+        if (body_json['action'] != 'asc' and body_json['action'] != 'desc'):
+            self.throw_custom_error("Invalid action")
+            return
+        try:
+            int(body_json['quantity'])
+        except:
+            self.throw_custom_error("Invalid quantity")
+            return
+        
+        if(body_json['action']=='asc'):
+            contador += int(body_json['quantity'])
+        elif(body_json['action']=='asc'):
+            contador -= int(body_json['quantity'])
+
+        # Print the complete HTTP request
+        print("\n----- Incoming POST Request -----")
+        print(f"Requestline: {self.requestline}")
+        print(f"Headers:\n{self.headers}")
+        print(f"Body:\n{post_data.decode()}")
+        print("-------------------------------")
+
+        # Respond to the client
+        response_data = json.dumps({"message": "Received POST data", "data": post_data.decode()})
+        self._set_response("application/json")
+        self.wfile.write(response_data.encode())
+
+def run_server(server_class=HTTPServer, handler_class=MyHTTPRequestHandler, port=7800):
+    server_address = ("", port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Starting server on port {port}...")
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run_server()
